da463122d9b147c2a38bdf352135e365ca33f177
cjmlee <56055996+cjmlee@users.noreply.github.com>
2024-10-18 12:27:10 +0800

    Update CRISPR_Tileplot.r
    Syntax change

--- src/CRISPR_Tileplot.r
+++ src/CRISPR_Tileplot.r
@@ -8,58 +8,17 @@ require(RColorBrewer)
 
 rm(list = ls())
 
-setwd("C:/Users/lcj_m/OneDrive/Desktop/CRISPRScreen")
+###Set your working directory here where you save the folder##
+setwd("C:/Users/Mick Lee/Desktop/CRISPRTileplot")
 
-zscore <- read.table("zscore2.txt")
+zscore <- read.table("zscore2.txt",header=TRUE)
 zscore <- as.data.frame(zscore)
 z <- zscore
 
 
-### Tileplot of selected candidate gene of interest - Figure 1C ###
-##Example for ARID1A and JMJD1C##
+##########################################################################
 
-z$gene2=z$gene
-GOI<-c("ARID1A", "JMJD1C")
-list<-z %>% filter(gene%in%GOI)
-#list$gene2 = list$gene
-
-
-test<-ggplot(z, aes(zscore)) +
-  geom_tile( # geom_tile() draws rectangular colored areas
-    aes(
-      y = 1, # draw all tiles at the same y location
-      fill = after_stat(density)  # use computed density for fill
-    ),
-    stat = "density",
-    n = 200,
-    show.legend = F# number of points calculated by stat_density() 
-  )+scale_fill_gradient(low="white",high="black")+ scale_y_continuous(expand = c(0,0))
-p<-test+theme_bw()+theme(panel.grid=element_blank(),axis.text=element_blank(),axis.ticks=element_blank(),panel.background = element_blank(),
-                         panel.border = element_rect(colour = "black", fill=NA, size=1),
-                         plot.background = element_blank())+xlim(c(-3,3))+geom_vline(data = list, aes(xintercept=as.numeric(zscore)), color ="red", lwd=1, lty=1)+xlab("")+ylab("")+coord_equal()+facet_grid(list$gene2~., switch="y")
-
-plot = paste("zscore_enriched_ARID1A_JMJD1C","_",".png",sep="")
-png(plot,width=2000,height=500,res=300)
-pdf("zscore_ARID1A_JMJD1C.pdf")
-p
-
-dev.off()
-
-####PLOT non-targetting Controls###
-overall<-ggplot(list, aes(zscore)) +
-  geom_tile( # geom_tile() draws rectangular colored areas
-    aes(
-      y = 1, # draw all tiles at the same y location
-      fill = after_stat(density)  # use computed density for fill
-    ),
-    stat = "density",
-    n = 200,
-    show.legend = F# number of points calculated by stat_density() 
-  )+scale_fill_gradient(low="white",high="orange")+ scale_y_continuous(expand = c(0,0))+theme_bw()+theme(panel.grid=element_blank(),axis.text=element_blank(),panel.background = element_blank(), plot.background = element_blank())+xlim(c(-3,3))+xlab("")+ylab("")+theme(ggside.panel.border = element_blank())
-pdf("zscore_density_plot_Controls.pdf")
-overall
-dev.off()
-
+###Plotting the distribution of your library, setting X scale consistent to -3 to 3###
 
 plot = paste("zscore_density_plot","_",".png",sep="")
 png(plot,width=2000,height=500,res=300)
@@ -73,7 +32,58 @@ overall<-ggplot(z, aes(zscore)) +
     n = 200,
     show.legend = F# number of points calculated by stat_density() 
   )+scale_fill_gradient(low="white",high="black")+ scale_y_continuous(expand = c(0,0))+geom_xsidedensity(aes(y = after_stat(density)),show.legend = F)+theme_bw()+theme(panel.grid=element_blank(),axis.text=element_blank(),axis.ticks=element_blank(),panel.background = element_blank(), plot.background = element_blank())+xlim(c(-3,3))+xlab("")+ylab("")+theme(ggside.panel.border = element_blank())
-pdf("zscore_density_plot.pdf")
+
 overall
+
+dev.off()
+
+#########################################################################
+
+###############PLOT non-targetting Controls in orange#######################
+
+GOI_control="Control"
+NTC<-z %>% filter(gene%in%GOI_control)
+pdf("zscore_density_plot_Controls.pdf")
+overall<-ggplot(NTC, aes(zscore)) +
+  geom_tile( # geom_tile() draws rectangular colored areas
+    aes(
+      y = 1, # draw all tiles at the same y location
+      fill = after_stat(density)  # use computed density for fill
+    ),
+    stat = "density",
+    n = 200,
+    show.legend = F# number of points calculated by stat_density() 
+  )+scale_fill_gradient(low="white",high="orange")+ scale_y_continuous(expand = c(0,0))+theme_bw()+theme(panel.grid=element_blank(),axis.text=element_blank(),panel.background = element_blank(), plot.background = element_blank())+xlim(c(-3,3))+xlab("")+ylab("")+theme(ggside.panel.border = element_blank())
+
+overall
+dev.off()
+
+
+### Tileplot of selected candidate gene of interest - Figure 1C ###
+##Example for ARID1A and JMJD1C##
+
+
+GOI<-c("ARID1A", "JMJD1C")
+list<-z %>% filter(gene%in%GOI)
+
+######Example for to plot Selected genes ###############
+##Make sure you keep the x-scale consistent
+
+
+GOI<-c("ARID1A", "JMJD1C")
+list<-z %>% filter(gene%in%GOI)
+
+##Export plot in png ##
+plot = paste("zscore_enriched_ARID1A_JMJD1C","_",".png",sep="")
+png(plot,width=2000,height=500,res=300)
+p<-test+theme_bw()+theme(panel.grid=element_blank(),axis.text=element_blank(),axis.ticks=element_blank(),panel.background = element_blank(),
+                         panel.border = element_rect(colour = "black", fill=NA, size=1),
+                         plot.background = element_blank())+xlim(c(-3,3))+geom_vline(data = list, aes(xintercept=as.numeric(zscore)), color ="red", lwd=1, lty=1)+xlab("")+ylab("")+coord_equal()+facet_grid(list$gene~., switch="y")
+p
+
+
+
+
+
 dev.off()
 
